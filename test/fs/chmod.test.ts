69b5cff0516666c0fdb25b713230c25c03777ef7
Baptiste Augrain <daiyam@zokugun.org>
2026-01-17 23:07:31 +0100

    test: add chmod tests

---
+++ test/fs/chmod.test.ts
@@ -0,0 +1,71 @@
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
+import klawSync from 'klaw-sync';
+import { afterEach, beforeEach, expect, it } from 'vitest';
+import fse from '../../src/index.js';
+
+const TEST_DIR = path.join(os.tmpdir(), 'test-fs-extra', 'fs-chmod');
+
+beforeEach(async () => {
+	await fse.emptyDirAsync(TEST_DIR);
+});
+
+afterEach(async () => {
+	for(const data of klawSync(TEST_DIR)) {
+		fs.chmodSync(data.path, 0o777);
+	}
+
+	await fse.removeAsync(TEST_DIR);
+});
+
+it('666', () => {
+	const file = path.join(TEST_DIR, 'some-file.txt');
+	fse.outputFileSync(file, 'hello');
+	fse.chmodSync(file, 0o666);
+
+	const readResult = fse.readFileSync(file, 'utf8');
+	expect(readResult.fails).to.be.false;
+	expect(readResult.value).to.equals('hello');
+
+	const writeResult = fse.writeFileSync(file, 'hello world');
+	expect(writeResult.fails).to.be.false;
+});
+
+it('600', () => {
+	const file = path.join(TEST_DIR, 'some-file.txt');
+	fse.outputFileSync(file, 'hello');
+	fse.chmodSync(file, 0o600);
+
+	const readResult = fse.readFileSync(file, 'utf8');
+	expect(readResult.fails).to.be.false;
+	expect(readResult.value).to.equals('hello');
+
+	const writeResult = fse.writeFileSync(file, 'hello world');
+	expect(writeResult.fails).to.be.false;
+});
+
+it('444', () => {
+	const file = path.join(TEST_DIR, 'some-file.txt');
+	fse.outputFileSync(file, 'hello');
+	fse.chmodSync(file, 0o444);
+
+	const readResult = fse.readFileSync(file, 'utf8');
+	expect(readResult.fails).to.be.false;
+	expect(readResult.value).to.equals('hello');
+
+	const writeResult = fse.writeFileSync(file, 'hello world');
+	expect(writeResult.fails).to.be.true;
+});
+
+it('222', () => {
+	const file = path.join(TEST_DIR, 'some-file.txt');
+	fse.outputFileSync(file, 'hello');
+	fse.chmodSync(file, 0o222);
+
+	const readResult = fse.readFileSync(file, 'utf8');
+	expect(readResult.fails).to.be.true;
+
+	const writeResult = fse.writeFileSync(file, 'hello world');
+	expect(writeResult.fails).to.be.false;
+});
